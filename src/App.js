25e4859875434e6f3155d9441729820889c79f59
willdharris <66787651+willdharris@users.noreply.github.com>
2021-03-04 20:29:36 -0500

    added create user function to store users in firestore

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -23,9 +23,8 @@ class App extends React.Component {
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-
-      console.log(user);
+      // this.setState({ currentUser: user });
+      createUserProfileDocument(user);
     });
   }
 
