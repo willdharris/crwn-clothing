bfb90c49563762a222e37f76a948acd251bb4cb7
willdharris <66787651+willdharris@users.noreply.github.com>
2021-03-07 15:59:18 -0500

    further implemented firebase utils to store authenticated users

--- src/App.js
+++ src/App.js
@@ -22,9 +22,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      // this.setState({ currentUser: user });
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUP from "../../components/sign-up/sign-up.component";
 
 import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUP />
   </div>
 );
 
