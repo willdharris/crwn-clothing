f58de44e309732c52d43d27fe878b2d426edb71f
willdharris <66787651+willdharris@users.noreply.github.com>
2021-04-24 15:35:21 -0400

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store";
 
 import "./index.css";
 import App from "./App";
@@ -11,9 +13,11 @@ import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <PersistGate persistor={persistor}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
